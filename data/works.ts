31f37a6788cc6e9497cac1e48e7ec1b09af419cd
Agility6 <agility1013@gmail.com>
2026-06-28 13:07:29 +0800

    feat: update protos

--- data/works.ts
+++ data/works.ts
@@ -3,4 +3,14 @@ import type { Work } from "@/lib/types";
 export const works: Work[] = [
   { id: "dalian-threshold", image: "/photos/DSC_1391.JPG", year: "2025" },
   { id: "hk", image: "/photos/DSC_2265.JPG", year: "2026" },
+  { id: "0620_1", image: "/photos/DSC_0060.JPG", year: "2026" },
+  { id: "0620_2", image: "/photos/DSC_0082.JPG", year: "2026" },
+  { id: "0620_3", image: "/photos/DSC_0243.JPG", year: "2026" },
+  { id: "0620_4", image: "/photos/DSC_0311.JPG", year: "2026" },
+  { id: "0620_5", image: "/photos/DSC_0461.JPG", year: "2026" },
+  { id: "0620_6", image: "/photos/DSC_0345.JPG", year: "2026" },
+  { id: "0620_7", image: "/photos/DSC_0278.JPG", year: "2026" },
+  { id: "0620_8", image: "/photos/DSC_0492.JPG", year: "2026" },
+  { id: "0326_1", image: "/photos/DSC_2536.JPG", year: "2026" },
+  { id: "0219_1", image: "/photos/DSC_2225.JPG", year: "2026" },
 ];
