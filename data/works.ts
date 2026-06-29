f0595bea01dd8274b49b1366efab742bbcc3665a
Agility6 <agility1013@gmail.com>
2026-06-29 14:14:14 +0800

    feat: update photo

--- data/works.ts
+++ data/works.ts
@@ -8,9 +8,10 @@ export const works: Work[] = [
   { id: "0620_3", image: "/photos/DSC_0243.JPG", year: "2026" },
   { id: "0620_4", image: "/photos/DSC_0311.JPG", year: "2026" },
   { id: "0620_5", image: "/photos/DSC_0461.JPG", year: "2026" },
-  { id: "0620_6", image: "/photos/DSC_0345.JPG", year: "2026" },
-  { id: "0620_7", image: "/photos/DSC_0278.JPG", year: "2026" },
-  { id: "0620_8", image: "/photos/DSC_0492.JPG", year: "2026" },
+  { id: "0620_6", image: "/photos/DSC_0309.jpeg", year: "2026" },
+  { id: "0620_7", image: "/photos/DSC_0345.JPG", year: "2026" },
+  { id: "0620_8", image: "/photos/DSC_0278.JPG", year: "2026" },
+  { id: "0620_9", image: "/photos/DSC_0492.JPG", year: "2026" },
   { id: "0326_1", image: "/photos/DSC_2536.JPG", year: "2026" },
   { id: "0219_1", image: "/photos/DSC_2225.JPG", year: "2026" },
 ];
